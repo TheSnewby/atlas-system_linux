81d47e38a555165fbd5dd279182f05d6fdb98849
TheSnewby <stephen.j.newby@gmail.com>
2025-01-08 09:44:25 -0600

    moved files into the new directory getline

---
+++ getline/laps.c
@@ -0,0 +1,12 @@
+#include <laps.h>
+/**
+ * race_state - keeps track of the number of laps made by several cars in a race.
+ * @id: int representing identifier of each car
+ * @size: number of cars, size of the array
+ * 
+ * Return: void
+ */
+
+void race_state(int *id, size_t size) {
+
+}
