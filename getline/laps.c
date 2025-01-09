7e51e6db31e39381b01c98315e70ebaaed67b829
TheSnewby <stephen.j.newby@gmail.com>
2025-01-09 14:55:51 -0600

    0-racing cars

--- getline/laps.c
+++ getline/laps.c
@@ -1,6 +1,8 @@
-#include <laps.h>
+#include "laps.h"
 /**
- * race_state - keeps track of the number of laps made by several cars in a race.
+ * race_state - keeps track of the number of laps made by several cars in a race
+ * The number of laps of each cars listed in id must be incremented by 1.
+ * Print the state of the race.
  * @id: int representing identifier of each car, pointer to int array
  * @size: number of cars, size of the array
  * 
@@ -8,5 +10,46 @@
  */
 
 void race_state(int *id, size_t size) {
+	static int ids[10] = {0};
+	static int lap[10] = {0};
+	static int ids_size = 10;
+	static int current_size = 0;
+	int i, j, id_found;
 
+	if (size == 0) {
+		printf("free all");
+		return;
+	}
+
+	/* Search for each id in the list */
+	for (i = 0; i < size; i++) {
+		id_found = 0;  /* not found */
+		for (j = 0; j < ids_size; j++) {
+			if (id[i] == ids[j]) {
+				id_found = 1; /* found */
+			}
+			if (id_found) {  /* increment lap count */
+				lap[j]++;
+				break;
+			}
+		}
+		if (!id_found) {  /* add new vehicle to the list */
+			printf("Car %d joined the race\n", id[i]);
+			if (j < ids_size - 1) {  /* makes sure new car fits in memory */
+				ids[current_size] = id[i];
+				current_size++;
+			}
+			/* insert resize or new static array here for larger size*/
+		}
+	}
+
+	/* Prints the race state */
+	printf("Race state:\n");
+	for (j = 0; j < current_size; j++) {
+		printf("Car %d [%d laps]", ids[j], lap[j]);
+	}
 }
+
+// car *add_car(car **head, int id, int lap) {
+
+// }
--- getline/laps.h
+++ getline/laps.h
@@ -1,7 +1,20 @@
-#ifndef laps.h
-#define laps.h
-
+#ifndef laps_h
+#define laps_h
 #include <stdio.h>
+// #include <stdlib.h>
 
+/**
+ * car - singly linked list of cars
+ * @id: id of car
+ * @lap: lap number
+ * @next: points to the next car/node
+ */
+typedef struct car {
+	int id;
+	int lap;
+	struct car *next;
+} car;
+
+void race_state(int *id, size_t size);
 
 #endif
