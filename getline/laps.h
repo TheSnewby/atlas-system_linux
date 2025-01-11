94bec8f67ad644ea472be46884ac74e75ed20fc8
TheSnewby <stephen.j.newby@gmail.com>
2025-01-11 14:47:56 -0600

    bettified lap.h

--- getline/laps.h
+++ getline/laps.h
@@ -1,19 +1,6 @@
 #ifndef laps_h
 #define laps_h
 #include <stdio.h>
-// #include <stdlib.h>
-
-/**
- * car - singly linked list of cars
- * @id: id of car
- * @lap: lap number
- * @next: points to the next car/node
- */
-// typedef struct car {
-// 	int id;
-// 	int lap;
-// 	struct car *next;
-// } car;
 
 void race_state(int *id, size_t size);
 void add_new_vehicle(int id, int current_size, int *ids, int *lap);
