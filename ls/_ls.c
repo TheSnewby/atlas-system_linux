9200c26f2fcfdad62ac1ecb9427cee9273451b24
TheSnewby <stephen.j.newby@gmail.com>
2025-01-20 15:56:23 -0600

    moved print code to its own function

--- ls/_ls.c
+++ ls/_ls.c
@@ -22,6 +22,46 @@
  * ./hls to run
  */
 
+/**
+ * print_dir - prints the passed directory
+ * @argc: number of arguments
+ * @directory: string of directory
+ * @options: options array where [0] = 1 means long (-l) and [1] = all (-a)
+ *
+ * Return: void
+ */
+void print_dir(int argc, char *directory, int *options)
+{
+	int i;
+	struct dirent *entry;
+	DIR *dir;
+
+	if (argc == 1)
+		if ((dir = opendir(".")) == NULL)
+			fprintf(stderr, "opendir failure in print_dir\n");
+	else
+		if ((dir = opendir(directory)) == NULL)
+			fprintf(stderr, "opendir failure in print_dir\n");
+
+	if (options[0] == 0 && options[1] == 0)
+	{
+		while((entry = readdir(dir)) != NULL)
+		{
+			if (entry->d_name[0] != '.')
+				printf("%s\t", entry->d_name);  /* might need formatting rework? */
+		}
+		printf("\n");
+	}
+	else if (options[0] == 0 && options[1] == 1)  /* only -a */
+	{
+		while((entry = readdir(dir)) != NULL)  /* prints all of directory */
+			printf("%s\t", entry->d_name);  /* might need formatting rework? */
+		printf("\n");
+	}
+
+	if (closedir(dir) < 0)
+		fprintf(stderr, "closedir failure in print_dir\n");
+}
 
 /**
  * parse_options - accumulates all options
@@ -45,12 +85,14 @@ int *parse_options(int argc, char **argv)
 				switch(argv[i][j]) {
 					case 'l':
 						options[0] = 1;  /* sets long (-l) option */
+						printf("L FOUND!\n");
 						break;
 					case 'a':
 						options[1] = 1;  /* sets all (-a) option */
+						printf("A FOUND!\n");
 						break;
 					default:  /* if unrecognized, print error */
-						fprintf(stderr, "ls: invalid option -- %c", argv[i][j]);
+						fprintf(stderr, "ls: invalid option -- %c\n", argv[i][j]);
 				}
 			}
 		}
@@ -66,47 +108,28 @@ int *parse_options(int argc, char **argv)
  *
  * Return: string of formatted text in pwd
  */
-char *_ls(int argc, char **argv)  /* argv[0] is program hls */
+char *_ls(int argc, char **argv)  /* argv[0] is program hls, consider moving all to main */
 {
-	int i, option_start_index;
-	char *cmd = "ls";
-	int closedir_rtn;
-	char cwd[PATH_MAX];
-	DIR *dir;
-	struct dirent *entry;
+	int i, j;
 	int *options;
-	// DIR *dirs[] = NULL;
+	char directory[PATH_MAX];
 
-	for (i = 0; i < argc; i++)
-		printf("argv[%d]: %s\n", i, argv[i]);
+	options = parse_options(argc, argv);
 
-	if (argc >= 2)
-		options = parse_options(argc, argv);
-	else
-		// options = [0, 0];
-
-	/* set cwd equal to "." if none given */
-
-	/* FIGURE OUT DIRECTORY */
-	/* concat directories to "./" */
-
-	if ((dir = opendir(".")) == NULL)
+	if (argc == 1)  /* default no arguments */
+		print_dir(argc, ".", options);
+	else  /* iterate through arguments and print dirs */
 	{
-		printf("opendir failure\n");
-		return (NULL);
+		for (i = 1; i < argc; i++)
+		{
+			if (argv[i][0] != '-')
+			{
+				sprintf(directory, "%s%s", "./", argv[i]); 
+				print_dir(argc, directory, options);
+				sprintf(directory, "./");  /* reset directory, memset not allowed */
+			}
+		}
 	}
-
-
-	while((entry = readdir(dir)) != NULL)  /* prints contents of directory */
-		printf("%s\t", entry->d_name);
-	printf("\n");
-	if (closedir(dir) < 0)
-	{
-		printf("closedir failure\n");
-		return (NULL);
-	}
-
-
 	return (NULL);
 }
 
