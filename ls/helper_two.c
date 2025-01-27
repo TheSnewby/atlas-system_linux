9b3d799c9c04a82f948d418c1ee91a2d6c665a62
“internashionalist” <taylor.thames@atlasschool.com>
2025-01-27 14:34:15 -0600

    corrected documentation

--- ls/helper_two.c
+++ ls/helper_two.c
@@ -32,23 +32,22 @@ char *adjust_long_time(char *str)
 
 /**
  * print_error - prints errors
- * @msg_num: int denoting which message to print
+ * @msg_num: error type (1 = non-fatal, 2 = fatal)
  * @program_name: name of program
- * @file_path: file path
+ * @file_path: file path where error occurred
  * @errnum: errno of error
- * @str_one: possible malloc'd str that needs freeing
- * @str_two: possible malloc'd str that needs freeing
+ * @str_one: string that needs freeing
+ * @str_two: string that needs freeing
  *
- * Return: 0 if it shouldn't quit, or passed errno to exit ?
  */
 void print_error(int msg_num, char* program_name, char *file_path, int errnum,
 char *str_one, char *str_two)
 {
     /* figure out which message should be printed */
     if (errnum == 2) /* file not found */
-        fprintf(stderr, "%s: cannot access %s: ", program_name, file_path);
+        fprintf(stderr, "%s: cannot access '%s': ", program_name, file_path);
     else if (errnum == 13) /* permission denied */
-        fprintf(stderr, "%s: cannot open directory %s: ", program_name, file_path);
+        fprintf(stderr, "%s: cannot open directory '%s': ", program_name, file_path);
 
     perror(NULL); /* system error message */
 
@@ -58,7 +57,7 @@ char *str_one, char *str_two)
     if (str_two)
         free(str_two);
 
-	/* fatal errors */
+	/* fatal errors (exit)*/
     if (msg_num == 2)
         exit(errnum);
 }
