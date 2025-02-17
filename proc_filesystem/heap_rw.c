deb11d364ea4c5637d4a7542e984f2dc9f0dc463
TheSnewby <stephen.j.newby@gmail.com>
2025-02-16 22:25:37 -0600

    first attempt at word replace for a normal call

---
+++ proc_filesystem/heap_rw.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <string.h>
+
+/**
+ * heap_rw - opens a memory address, adjusted permissions, and replaces a value
+ * @pid: process id
+ * @mem_begin: beginning of memory range of heap in PID
+ * @mem_end: end of memory range of heap in PID
+ *
+ * Return: 0 is successful, other if error
+ */
+int heap_rw(int pid, char *mem_begin, char *mem_end, char *find, char *replace)
+{
+	int ptrace_rtn = 0;
+	size_t word_size = sizeof(long);
+	char data[word_size];
+	long word;
+	unsigned int addr;
+
+	printf("find: %s\nreplace: %s\n", find, replace);
+
+	ptrace_rtn = ptrace(PTRACE_ATTACH, pid, 0, 0);
+	if (ptrace_rtn == -1)
+	{
+		fprintf(stderr, "Error in ptrace attach: ");
+		perror(NULL);
+	}
+
+	for (addr = (long)mem_begin; addr <= (long)mem_end; addr += word_size)
+	{
+		word = ptrace(PTRACE_PEEKDATA, pid, mem_begin, NULL);
+		if (word == -1)
+		{
+			fprintf(stderr, "Error in ptrace peek: ");
+			perror(NULL);
+			break;
+		}
+		else
+		{
+			printf("word: %s\n", (char *) word);
+			if (strcmp(find, (char *)word) == 0)
+			{
+				printf("find found!\n");
+				ptrace_rtn = ptrace(PTRACE_POKEDATA, pid, addr, replace);  /* should (long)replace? */
+				if (ptrace_rtn == -1)
+				{
+					fprintf(stderr, "Error in ptrace poke: ");
+					perror(NULL);
+				}
+			}
+			else if (strstr(find, (char *) word) != NULL)  /* find in word */
+			{
+				printf("find in word!\nFigure out what to do.\n");
+			}
+			/* have a case for *find being found in across multiple words? */
+		}
+	}
+
+	ptrace_rtn = (PTRACE_DETACH, pid, 0, 0);  /* consider adding this to all errors */
+	if (ptrace_rtn == 0)
+		printf("Detached Successfully!\n");
+	return (0);
+}
+
+// int main(void)
+// {
+// 	heap_rw(610, "5570452c3000", "5570452e4000", "hi", "goodbye");
+// 	return 0;
+// }
--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
 """Utilize the /proc system to access read, write, heap info for a given PID"""
+from sys import argv
+from ctypes import *
+import os
 
 def get_heap_address_range(pid:int) -> tuple[int, int]:
 	"""returns the heap address range of a given PID"""
@@ -27,9 +29,27 @@ def get_heap_address_range(pid:int) -> tuple[int, int]:
 	print(heap_range)
 	return heap_range
 
-def find_and_replace(heap_range: tuple[int, int], find: str, replace: str) -> None:
+def find_and_replace(pid: int, heap_range: tuple[int, int], find: str, replace: str):
 	"""finds and replaces a value in the memory range"""
-	pass
+	heap_rw_lib = CDLL("./heap_rw.so")  # comment these three lines out when uncommenting the other lines
+	heap_rw_lib.heap_rw.argtypes = [c_int, c_char_p, c_char_p, c_char_p, c_char_p]
+	heap_rw_lib.heap_rw(pid, heap_range[0], heap_range[1], find, replace)
+	# path = f"proc/{pid}/mem"
+	# try:
+	# 	with open(path) as file:
+	# 		print(file)
+
+	# except PermissionError:
+	# 	print("Error: Permission denid. Please check file permissions.")
+	# 	heap_rw_lib = CDLL("./heap_rw.so")
+	# 	heap_rw_lib.heap_rw.argtypes = [c_int, c_char_p, c_char_p, c_char_p, c_char_p]
+	# 	heap_rw_lib.heap_rw(pid, heap_range[0], heap_range[1], find, replace)
+	# except FileNotFoundError:
+	# 	print("Error: File not found.")
+	# 	exit(1)
+	# except Exception as e:
+	# 	print(f"An unexpected error occurred: {e}")
+	# 	exit(1)
 
 if __name__ == "__main__":
 	if len(argv) != 4:
