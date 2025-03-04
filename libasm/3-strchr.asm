c5c5161e11e8a4cfb6f2e170f22b4b0829adea47
TheSnewby <stephen.j.newby@gmail.com>
2025-03-04 09:58:24 -0600

    3-strchr

---
+++ libasm/3-strchr.asm
@@ -0,0 +1,32 @@
+; strchr
+; @rdi: string
+; @rsi: character
+;
+; returns: memory location of character in string in rax, or NULL if not found
+
+section .text
+	global asm_strchr
+
+asm_strchr:
+	; while not at null byte, check for character at location
+	; return location if found, otherwise return 0?
+.loop:
+	mov al, byte [rdi]
+
+	cmp al, 0
+	je .notfound
+
+	cmp al, sil ; sil is 8-bit register of rsi
+	je .found
+
+	inc rdi
+
+	jmp .loop
+
+.notfound:
+	xor rax, rax
+	ret
+
+.found:
+	mov rax, rdi
+	ret
