1597e0b96cb86920e473fca28ff0e07c42b26b2b
TheSnewby <stephen.j.newby@gmail.com>
2025-03-03 19:01:40 -0600

    1-strcmp almost there

---
+++ libasm/1-strcmp.asm
@@ -0,0 +1,45 @@
+; strcmp()
+; @rdi: first  param
+; @rsi: second param
+; return: rax an integer less than, equal to, or greater than zero if s1 
+; 	(or the first n bytes thereof) is found, respectively,  to  be  less  than,
+;	to match, or be greater than s2
+BITS 64
+global asm_strcmp
+
+section .text:
+
+asm_strcmp:
+.loop:
+	mov al, [rdi]
+	mov bl, [rsi]
+
+	cmp al, bl
+	jne .notequal
+
+	test al, al ; test is 0 if rdi is a null byte
+	je .equal ; jump if equal
+
+	inc rdi
+	inc rsi
+	jmp .loop
+
+.notequal:
+	movzx eax, byte [rdi] ; zero extended to handle signed integer
+	movzx ebx, byte [rsi]
+	sub eax, ebx
+	cmp eax, 0
+	jg .greater
+	jl .lesser
+
+.greater:
+	mov eax, 1
+	ret
+
+.lesser:
+	mov eax, -1
+	ret
+
+.equal:
+	xor eax, eax
+	ret
