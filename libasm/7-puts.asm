a1937fd39fd9e502e46d9f9055c04273a09e790f
TheSnewby <stephen.j.newby@gmail.com>
2025-03-09 00:58:01 -0600

    preserved rdi register to protect its value from cross-function changes

--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -8,18 +8,14 @@ section .text
 	global asm_puts
 
 asm_puts:
-	call asm_strlen			; length of string
-	cmp rax, 0
-	; cmovl rdx, -1				; if <= 0, return an error
-	; cmovge rdx, rax			; store the length into the string counter register
+	push rdi				; saves 
+	call asm_strlen
 	mov rdx, rax
 
-	mov rsi, rdi			; load memory location into source pointer
-
+	pop rsi
 	mov rax, 1				; Write syscall
 	mov rdi, 1				; write to StdOut
 	syscall
 
-	mov rax, rdx
 	ret
 
