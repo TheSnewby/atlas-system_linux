ee28c62faa284bba199fd38452f3437e3574a71b
TheSnewby <stephen.j.newby@gmail.com>
2025-03-17 18:46:03 -0500

    adjusted printing funciton

--- signals/2-handle_sigaction.c
+++ signals/2-handle_sigaction.c
@@ -8,8 +8,7 @@
  */
 void sigint_handler(int sig)
 {
-	if (sig == SIGINT)
-		printf("Gotcha! [%d]\n", sig);
+	printf("Gotcha! [%d]\n", sig);
 	fflush(NULL);
 }
 
@@ -20,12 +19,9 @@ void sigint_handler(int sig)
  */
 int handle_sigaction(void)
 {
-	//int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
 	struct sigaction newact;
 
 	newact.sa_handler = sigint_handler;
-	/* sigemptyset(&newact.sa_mask); */
-	/* newact.sa_flags = 0; */
 
 	if (sigaction(SIGINT, &newact, NULL) == -1)
 		return (-1);
