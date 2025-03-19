478506bc0dd8915a3d28aef873358bcb5f12077c
TheSnewby <stephen.j.newby@gmail.com>
2025-03-19 18:29:29 -0500

    synta

--- signals/8-signal_send.sh
+++ signals/8-signal_send.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # sends the signal SIGQUIT to a process, given its PID
 
-if [ $1 -ne 1]; then
+if [ $1 -ne 1 ]; then
 	echo "Usage: $0 <pid>"
 	exit 1
 fi
 
 if [[ $1 =~ ^[0-9]+$ ]]; then
-	kill -3 -$1
+	kill -3 $1
 	exit 0
 else
 	exit 1
