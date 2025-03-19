81aa0c4447a5f87a75c5711d54ea2e86c8f03fd9
TheSnewby <stephen.j.newby@gmail.com>
2025-03-18 22:16:32 -0500

    5-signal_describe

---
+++ signals/5-signal_describe.c
@@ -0,0 +1,20 @@
+#include "signals.h"
+
+/**
+ * main - start of program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if successful, other if not
+ */
+int main(int argc, char **argv)
+{
+	int signum = atoi(argv[1]);
+
+	if (argc != 2)
+		pritnf("Usage: %s %d\n", argv[0], signum);
+
+	printf("%d: %s\n", signum, strsignal(signum));
+
+	return (0);
+}
