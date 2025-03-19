b4273120d85c9dcf19cf36ffb3f5a073c89683b1
TheSnewby <stephen.j.newby@gmail.com>
2025-03-19 18:25:25 -0500

    8-sh

---
+++ signals/8-signal_send.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# sends the signal SIGQUIT to a process, given its PID
+
+if [ $1 -ne 1]; then
+	echo "Usage: $0 <pid>"
+	exit 1
+fi
+
+if [[ $1 =~ ^[0-9]+$ ]]; then
+	kill -3 -$1
+	exit 0
+else
+	exit 1
+fi
