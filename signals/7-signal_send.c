33a4d3cf2fd8dfeff643e29cc4ae144a84891bde
TheSnewby <stephen.j.newby@gmail.com>
2025-03-19 13:50:39 -0500

    7-signal

---
+++ signals/7-signal_send.c
@@ -0,0 +1,22 @@
+#include "signals.h"
+
+/**
+ * main - sends the signal SIGINT to a process, given its PID
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE otherwise
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s %s\n", argv[0], argv[1]);
+		return (EXIT_FAILURE);
+	}
+
+	if (pidfd_send_signal(atoi(argv[1]), SIGINT, NULL, 0))
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
