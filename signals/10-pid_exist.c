ccf496248686694081fa9324d56936875d1166d6
TheSnewby <stephen.j.newby@gmail.com>
2025-03-19 19:17:20 -0500

    10=-pid

--- signals/10-pid_exist.c
+++ signals/10-pid_exist.c
@@ -1,1 +1,15 @@
-#include "signals.h"
+#include <stdlib.h>
+#include <signal.h>
+//only two headers
+
+/**
+ * main - tests if a process exists, given its PID
+ * @argc: number of arguments
+ * @argv: array of strings
+ *
+ * Return: 1 if pid exists, 0 otherwise
+ */
+int main(int argc, char **argv)
+{
+	return (kill(atoi(argv[1]), 0) == 0) ? (1) : (0);
+}
