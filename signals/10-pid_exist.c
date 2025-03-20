eede1e755f50c731717bca679153dd013817d8da
TheSnewby <stephen.j.newby@gmail.com>
2025-03-19 19:38:57 -0500

    added prototype to signals.h even though it's not used in the functino

--- signals/10-pid_exist.c
+++ signals/10-pid_exist.c
@@ -5,9 +5,9 @@
  * pid_exist - tests if a process exists, given its PID
  * @pid: process id
  *
- * Return: 1 if pid exists, 0 otherwise
+ * Return: 1 if process exists, 0 otherwise
  */
 int pid_exist(pid_t pid)
 {
-	return (kill(pid, 0) == 0);
+	return ((kill(pid, 0) == 0) ? 1 : 0);
 }
