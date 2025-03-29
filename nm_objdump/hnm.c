8e9be0c3a91eaee3d2d599f227a734b11ebb0ede
TheSnewby <stephen.j.newby@gmail.com>
2025-03-29 18:15:15 -0500

    changed local to global checks, removed notype check

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -90,7 +90,7 @@ char get_symbol_type_32(Elf32_Sym *sym, Elf32_Shdr *sections, char *shstrtab)
 	if (type == STT_FILE)
 		return (0);
 
-	if ((type == STT_NOTYPE) || (sym->st_shndx == SHN_UNDEF))
+	if ((sym->st_shndx == SHN_UNDEF)) //(type == STT_NOTYPE) || 
 		return ('U');
 
 	if (binding == STB_WEAK)
@@ -130,13 +130,13 @@ char get_symbol_type_32(Elf32_Sym *sym, Elf32_Shdr *sections, char *shstrtab)
 		return ((binding == STB_GLOBAL) ? 'T' : 't');
 
 	if ((strcmp(name, ".data") == 0) || ((section->sh_flags & SHF_ALLOC) && (section->sh_flags & SHF_WRITE)))
-		return ((binding == STB_LOCAL) ? 'D' : 'd');
+		return ((binding == STB_GLOBAL) ? 'D' : 'd');
 
 	if (strcmp(name, ".bss") == 0)
-		return ((binding == STB_LOCAL) ? 'B' : 'b');
+		return ((binding == STB_GLOBAL) ? 'B' : 'b');
 
 	if ((strcmp(name, ".rodata") == 0) || (section->sh_flags & SHF_ALLOC))
-		return ((binding == STB_LOCAL) ? 'R' : 'r');
+		return ((binding == STB_GLOBAL) ? 'R' : 'r');
 
 	return ('?');
 	// return (0);
