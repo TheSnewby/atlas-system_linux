70409f0f724cb31acdc233ba18f7fff322a69219
TheSnewby <stephen.j.newby@gmail.com>
2025-03-29 20:52:31 -0500

    supressing addresses from being printing for weak links

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -467,7 +467,7 @@ int parse_symbol_table(const char *file_path)
                 /* Print Symbol */
                 if ((name) && (symbol_type != '\0') && (strcmp(name, "") != 0))
 				{
-					if (!symbols_32[i].st_value || symbol_type == 'U')
+					if (!symbols_32[i].st_value || symbol_type == 'U' || symbol_type == 'w')
                         printf("         %c %s\n", symbol_type, name);
                     else
                         printf("%08x %c %s\n", symbols_32[i].st_value, symbol_type, name);
