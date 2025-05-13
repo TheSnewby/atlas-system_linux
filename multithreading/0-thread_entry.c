e1678d5c731a5d6607ee9127ddf41c1b463f6027
TheSnewby <stephen.j.newby@gmail.com>
2025-05-13 18:11:09 -0500

    removed main

--- multithreading/0-thread_entry.c
+++ multithreading/0-thread_entry.c
@@ -10,22 +10,3 @@ void *thread_entry(void *arg)
 	printf("%s\n", (char *)arg);
 	pthread_exit(arg);
 }
-
-/**
- * main - Entry point
- *
- * Return: EXIT_SUCCESS
- */
-int main(void)
-{
-    pthread_t tid;
-
-    pthread_create(&tid, NULL, &thread_entry, "Holberton School");
-
-    sleep(1);
-    printf("Created thread ID -> %lu\n", tid);
-
-    thread_entry("C is fun");
-    printf("Should not be printed\n");
-    return (EXIT_SUCCESS);
-}
