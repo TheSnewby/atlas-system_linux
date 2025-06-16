02f3dfe2c7807acbb0bc358bbae7661c2c42b6df
TheSnewby <stephen.j.newby@gmail.com>
2025-06-16 10:16:10 -0500

    refactored for betty

--- sockets/3-server.c
+++ sockets/3-server.c
@@ -9,6 +9,30 @@
 #include <string.h>
 
 /**
+ * serverRecv - waits for a message and then prints the message
+ * @clientfd: fd of the client
+ */
+void serverRecv(int clientfd)
+{
+	int flags = 0, bytesRecv = 0; /* for recv */
+	char recvBuf[1024]; /* message from recv */
+	size_t recvBufLen = 1024; /* length of recv message */
+
+	memset(recvBuf, 0, 1024);
+
+	bytesRecv = recv(clientfd, (void *)recvBuf, recvBufLen, flags);
+	if (bytesRecv == 1024)
+		printf("larger buffer required\n");
+	else if (bytesRecv < 0)
+	{
+		perror("Error in recv");
+		exit(EXIT_SUCCESS);
+	}
+	printf("Message received: \"");
+	printf("%.*s\"\n", bytesRecv, recvBuf);
+}
+
+/**
  * main - a program that opens an IPv4/TCP socket,
  * and listens to traffic on port 12345
  *
@@ -21,14 +45,11 @@ int main(void)
 	int domain = AF_INET, protocol = 0; /* IPv4 */ /* default protocol */
 	struct sockaddr_in addrport, clientaddrport;
 	socklen_t size = sizeof(struct sockaddr_in);
-	int backlog = 10, flags = 0, bytesRecv = 0; /* queue for accept; flags & bytesRecv for recv */
-	char recvBuf[1024]; /* message from recv */
-	size_t recvBufLen = 1024; /* length of recv message */
+	int backlog = 10; /* queue for accept */
 
 	addrport.sin_family = AF_INET; /* address family */
 	addrport.sin_port = htons(port); /* converts host ord to network byte ord */
 	addrport.sin_addr.s_addr = INADDR_ANY; /* sets ip addr to bind anything */
-	memset(recvBuf, 0, 1024);
 
 	fd = socket(domain, type, protocol); /* creates endpoint for communication */
 	if (fd < 0)
@@ -56,18 +77,7 @@ int main(void)
 
 	printf("Server listening on port %d\n", port);
 	printf("Client connected: %s\n", inet_ntoa(clientaddrport.sin_addr));
-
-	bytesRecv = recv(clientfd, (void *)recvBuf, recvBufLen, flags);
-	if (bytesRecv == 1024)
-		printf("larger buffer required\n");
-	else if (bytesRecv < 0)
-	{
-		perror("Error in recv");
-		exit(EXIT_SUCCESS);
-	}
-	printf("Message received: \"");
-	printf("%.*s\"\n", bytesRecv, recvBuf);
+	serverRecv(clientfd);
 	close(clientfd);
-
 	return (0);
 }
