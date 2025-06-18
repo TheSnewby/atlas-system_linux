63c2d04e16df2c9180a84d0c18e77d3768895dfa
TheSnewby <stephen.j.newby@gmail.com>
2025-06-17 23:05:15 -0500

    updated 3 for betty, testing for 4

--- sockets/4-restapi.c
+++ sockets/4-restapi.c
@@ -8,6 +8,8 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+#define BUFFER_SIZE 1024
+
 /**
  * serverRecvAndSend - waits for a message and then prints the message
  * @clientfd: fd of the client
@@ -15,10 +17,10 @@
 void serverRecvAndSend(int clientfd)
 {
 	int flags = 0, bytesRecv = 0; /* for recv */
-	char recvBuf[1024]; /* message from recv */
-	size_t recvBufLen = 1024; /* length of recv message */
+	char recvBuf[BUFFER_SIZE]; /* message from recv */
+	size_t recvBufLen = BUFFER_SIZE; /* length of recv message */
 
-	memset(recvBuf, 0, 1024);
+	memset(recvBuf, 0, BUFFER_SIZE);
 
 	bytesRecv = recv(clientfd, (void *)recvBuf, recvBufLen, flags);
 	if (bytesRecv == 1024)
@@ -34,13 +36,13 @@ void serverRecvAndSend(int clientfd)
 
 /**
  * main - a program that opens an IPv4/TCP socket,
- * and listens to traffic on port 12345
+ * and listens to traffic on port 8080
  *
  * Return: 0
  */
 int main(void)
 { /* fd is also the sockid, port assigned */
-	int fd, clientfd = -2, port = 12345;
+	int fd, clientfd = -2, port = 8080;
 	int type = SOCK_STREAM; /* full-duplex byte stream */
 	int domain = AF_INET, protocol = 0; /* IPv4 */ /* default protocol */
 	struct sockaddr_in addrport, clientaddrport;
@@ -78,6 +80,11 @@ int main(void)
 	printf("Server listening on port %d\n", port);
 	printf("Client connected: %s\n", inet_ntoa(clientaddrport.sin_addr));
 	serverRecvAndSend(clientfd);
+	/* Print the full received HTTP request
+	Print the break-down of the first line of the received HTTP request (See example)
+	Send back a response to the connected client (HTTP 200 OK)
+	Close the connection with the client
+	Wait for the next connection */
 	close(clientfd);
 	return (0);
 }
